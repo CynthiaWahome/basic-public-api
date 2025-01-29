0363955d69bed0f984e7dcccaad412b472921c78
Cynthia <160864573+CynthiaWahome@users.noreply.github.com>
2025-01-29 21:54:37 +0300

    Revert "Node.js Implementation of the Public API"

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify
+from datetime import datetime
+from flask_cors import CORS
+
+import os
+
+class Config:
+    DEBUG = True
+    HOST = "0.0.0.0"
+    PORT = int(os.environ.get("PORT", 5000))  # Use the PORT environment variable
+
+app = Flask(__name__)
+app.config.from_object(Config)
+CORS(app)  # Enable CORS
+
+@app.route("/", methods=["GET"])
+def home():
+    response = {
+        "current_datetime": datetime.utcnow().replace(microsecond=0).isoformat() + "Z",  # ISO 8601 UTC
+        "email": os.getenv("EMAIL"),
+        "github_url": "https://github.com/CynthiaWahome/basic-public-api"
+    }
+    return jsonify(response), 200
+
+if __name__ == "__main__":
+    app.run(host=app.config["HOST"], port=app.config["PORT"])
